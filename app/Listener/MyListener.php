39bc13cc4753e8f718ec5864d5a46836df9fc79d
victor.tseng <withvictor@gmail.com>
2023-05-27 10:54:28 +0800

    telegrame 通知

--- app/Listener/MyListener.php
+++ app/Listener/MyListener.php
@@ -9,6 +9,8 @@ use Psr\Container\ContainerInterface;
 use Hyperf\Event\Contract\ListenerInterface;
 use Hyperf\Framework\Event\BootApplication;
 use Hyperf\Framework\Event\MainWorkerStart;
+use Longman\TelegramBot\Telegram;
+use Longman\TelegramBot\Request;
 
 #[Listener]
 class MyListener implements ListenerInterface
@@ -26,8 +28,24 @@ class MyListener implements ListenerInterface
 
     public function process(object $event): void
     {
-      echo "Workvictorer #{} started.\n";
-      echo "Workvictorer #{} started.\n";
-      echo "Workvictorer #{} started.\n";
+      $key = '1363470980:AAFS9yudcF1lA8jQSoQbqngZ37HZKWVmc3o';
+      $chat_id = '687442178';
+      $bot_api_key  = $key;
+      $bot_username = 'nextav1bot';
+
+      try {
+          // Create Telegram API object
+          $telegram = new Telegram($bot_api_key, $bot_username);
+          \Longman\TelegramBot\Request::initialize($telegram);
+          \Longman\TelegramBot\Request::sendMessage([
+              'chat_id' => $chat_id,
+              'text'    => 'Your utf8 text 😜 ...',
+          ]);          // Handle telegram webhook request
+    
+      } catch (Longman\TelegramBot\Exception\TelegramException $e) {
+          // Silence is golden!
+          // log telegram errors
+         echo $e->getMessage();
+      }
     }
 }
